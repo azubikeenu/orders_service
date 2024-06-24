35408052fb4f2edfe64a90cb379f9d9875453de6
Enu Azubike <enuazubike88@gmail.com>
2024-06-24 19:53:02 +0100

    cleaned vendor service

--- src/services/vendor.service.ts
+++ src/services/vendor.service.ts
@@ -14,24 +14,5 @@ export class VendorService {
     this.vendorRepository = vendorRepository;
   }
 
-  async createVendor(createVendorInput: CreateVendorDto) {
-    if (await this.vendorRepository.doesVendorExists(createVendorInput.email))
-      throw createHttpError(StatusCodes.BAD_REQUEST, `Vendor with email : ${createVendorInput.email} already exists`)
-    const response = await this.vendorRepository.createVendor(createVendorInput);
-    return response;
-
-  }
-
-  async findVendorById(_id: string) {
-    const vendor = await this.vendorRepository.findById(_id)
-    if (!vendor) throw createHttpError(StatusCodes.NOT_FOUND, `vendor with id ${_id} does not exist`)
-    return vendor
-  }
-
-  async findAllVendors() {
-    const allVendors = await this.vendorRepository.filterVendors({})
-    return allVendors;
-  }
-
 
 }
